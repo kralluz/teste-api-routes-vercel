b630820fa47e412743dce4253a8d73b193f2f89f
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2024-01-31 23:51:21 -0300

    upateSaudação

--- src/pages/api/saudacao.js
+++ src/pages/api/saudacao.js
@@ -1,5 +1,20 @@
+// src/pages/api/acoes.js
 export default function handler(req, res) {
-    res.status(200).json({
-        mensagem: "Olá, Kralluz! Bem-vindo à sua API Next.js!",
-    });
+    switch (req.method) {
+        case "GET":
+            res.status(200).json({ mensagem: "GET realizado com sucesso!" });
+            break;
+        case "POST":
+            res.status(200).json({ mensagem: "POST realizado com sucesso!" });
+            break;
+        case "PUT":
+            res.status(200).json({ mensagem: "PUT realizado com sucesso!" });
+            break;
+        case "DELETE":
+            res.status(200).json({ mensagem: "DELETE realizado com sucesso!" });
+            break;
+        default:
+            res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+            res.status(405).end(`Método ${req.method} não permitido.`);
+    }
 }
